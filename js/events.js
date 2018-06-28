83b25d4556a662facc906c76238f72d4db4cab5b
Sara Baronsky <sbaronsk@bowdoin.edu>
2018-06-28 12:14:41 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,11 @@
 //define functions here
-
-$(document).ready(function(){
+function getIT(){
+  $('p').on("click", getIT(g){
+    if (g.which ==)
+}
+  
 
 // call functions here
+Define a function getIt that does not accept a parameter. The function should bind a click event to the p tag. When the paragraph is clicked, the function should alert "Hey!".
 
 });
